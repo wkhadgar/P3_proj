b0e3e9418105480b01e0b5888d2b22458ef4629a
wkhadgar <pauloxrms@gmail.com>
2023-02-15 21:57:50 -0300

    interface menus
    Signed-off-by: wkhadgar <pauloxrms@gmail.com>

--- system_main.py
+++ system_main.py
@@ -43,7 +43,7 @@ class Account:
         if curr >= 0:
 
             now = datetime.now().time()
-            limit_start_time = datetime.strptime("18:00", "%H:%M").time()
+            limit_start_time = datetime.strptime("21:00", "%H:%M").time()
             limit_end_time = datetime.strptime("4:00", "%H:%M").time()
 
             if has_time_limit:
@@ -58,7 +58,7 @@ class Account:
                     return False
 
             self.balance = curr
-            self.score -= amount * 0.1
+            self.score -= amount * 0.15
 
             print(f"Sacado R$ {amount:.2f} da conta, o novo total é de R${self.balance:.2f}")
             return True
@@ -128,7 +128,7 @@ class Transaction:
 
 
 class Bank:
-    def __init__(self, name, *, fee: float = 0):
+    def __init__(self, name: str, *, fee: float = 0):
         """
         Descreve um banco.
 
@@ -139,7 +139,7 @@ class Bank:
         :param fee: Taxa, opcional, que pode ser usada sobre as operações.
         """
 
-        self.name = name
+        self.name = name.strip()
         self.clients = {}
         self.clients_ammount = 0
         self.vault = 10000
@@ -161,7 +161,7 @@ class Bank:
             self.clients[client.cpf].accounts.pop(self.name)
             self.clients.pop(client.cpf)
             self.clients_ammount -= 1
-        except ValueError:
+        except KeyError:
             self.clients_ammount += 1
 
 
@@ -187,7 +187,7 @@ class System:
         print("Pessoas:")
         for p in list(self.people.values()):
             try:
-                print(f"\t {p.name}.")
+                print(f"\t {p.name} - CPF: {p.cpf};")
             except:
                 continue
 
@@ -197,6 +197,7 @@ class System:
                 print(f"\t {b.name}.")
             except:
                 continue
+        print()
 
     def create_bank(self, *, name: str, bank_fee: float):
         """
@@ -206,14 +207,17 @@ class System:
         """
 
         self.banks[name] = Bank(name, fee=bank_fee)
-        print("Banco criado com sucesso.")
+        print(f"Banco {name} criado com sucesso.")
 
     def remove_bank(self, *, name: str):
-        if self.banks[name].clients_ammount == 0:
-            self.banks.pop(name)
-            print(f"O banco '{name}' foi removido do sistema com sucesso!")
-        else:
-            print(f"Não foi possível excluir o banco, pois ainda há clientes cadastrados nele.")
+        try:
+            if self.banks[name].clients_ammount == 0:
+                self.banks.pop(name)
+                print(f"O banco '{name}' foi removido do sistema com sucesso!")
+            else:
+                print(f"Não foi possível excluir o banco, pois ainda há clientes cadastrados nele.")
+        except KeyError:
+            print("Banco não encontrado no sistema!")
 
     def create_person(self, *, name, cpf: int):
         """
@@ -224,7 +228,7 @@ class System:
         """
 
         self.people[cpf] = Person(name, cpf)
-        print("Pessoa cadastrada com sucesso.")
+        print(f"A pessoa {name} cadastrada com sucesso.")
 
     def remove_person(self, cpf: int):
         try:
@@ -234,15 +238,18 @@ class System:
             name = self.people[cpf].name
             self.people.pop(cpf)
             print(f"A pessoa '{name}' foi removida do sistema com sucesso!")
-        except ValueError:
+        except KeyError:
             print(f"A pessoa com o identificador {cpf} não foi encontrada no sistema.")
 
     def get_person_data(self, cpf: int):
-        print(f"\nDados da pessoa '{self.people[cpf].name}':")
-        for acc in list(self.people[cpf].accounts.keys()):
-            print(
-                f"\tValor no banco {acc} R${self.people[cpf].accounts[acc].balance:.2f}; Score relacionado: {self.people[cpf].accounts[acc].score:}")
-        print("\n")
+        try:
+            print(f"\nDados da pessoa '{self.people[cpf].name}':")
+            for acc in list(self.people[cpf].accounts.keys()):
+                print(
+                    f"\tValor no banco {acc} R${self.people[cpf].accounts[acc].balance:.2f}; Score relacionado: {self.people[cpf].accounts[acc].score:}")
+            print("\n")
+        except KeyError:
+            print("A pessoa não foi encontrada no sistema!")
 
     def sys_open_account(self, owner_id: int, bank: str):
         """
@@ -253,7 +260,7 @@ class System:
         """
 
         self.banks[bank].open_account(self.people[owner_id])
-        print("A conta foi criada com sucesso!")
+        print(f"A conta de {self.people[owner_id].name} foi criada em {bank} com sucesso!")
 
     def sys_deposit(self, *, cpf: int, bank: str, value: float):
         """
@@ -271,7 +278,7 @@ class System:
             print(f"{self.people[cpf].name} realizou uma operação de depósito em {bank}")
             total = self.people[cpf].accounts[bank].deposit(value)
             self.banks[bank].vault += value
-        except ValueError:
+        except KeyError:
             print("O banco ou a pessoa não existe.\n")
         print("________________________________________________________\n")
 
@@ -298,7 +305,7 @@ class System:
                 drawed = value
             else:
                 pass
-        except ValueError:
+        except KeyError:
             print("O banco ou a pessoa não existe.\n")
         print("________________________________________________________\n")
         return drawed
@@ -306,7 +313,7 @@ class System:
     def generate_transaction_id(self, transaction: Transaction):
         new_id = random.randint(0, 999_999_999)
 
-        while new_id in self.transactions:
+        while new_id in list(self.transactions.keys()):
             new_id = random.randint(0, 999_999_999)
 
         self.transactions[new_id] = transaction
@@ -344,7 +351,7 @@ class System:
                 self.sys_deposit(cpf=target_id, bank=target_bank, value=value)
             else:
                 pass
-        except ValueError:
+        except KeyError:
             print("Erro!!")
 
         return new_transaction
@@ -352,98 +359,143 @@ class System:
 
 sys = System()
 
-print("\nDemo de utilização sistema\n")
-main_bank = input("Digite o nome do primeiro banco a ser criado: ").strip()
-other_bank = input("Digite o nome do segundo banco a ser criado: ").strip()
-fee = 0.01
+do_demo = 0
+if do_demo:
+    print("\nDemo de utilização sistema:\n")
+    main_bank = input("Digite o nome do primeiro banco a ser criado: ").strip()
+    fee = 0.01
 
-sys.create_bank(name=main_bank, bank_fee=fee)
-sys.create_bank(name=other_bank, bank_fee=fee)
+    sys.create_bank(name=main_bank, bank_fee=fee)
 
-giver_name = input("Digite o nome da pessoa que vai realizar a transferencia: ")
-giver_cpf = int(input("Digite o CPF dessa pessoa: "))
+    other_bank = input("Digite o nome do segundo banco a ser criado: ").strip()
+    sys.create_bank(name=other_bank, bank_fee=fee)
 
-receiver_name = input("Digite o nome da pessoa que vai receber a transferencia: ")
-receiver_cpf = int(input("Digite o CPF dessa pessoa: "))
+    giver_name = input("Digite o nome da pessoa que vai realizar a transferencia: ")
+    giver_cpf = int(input("Digite o CPF dessa pessoa: "))
+    sys.create_person(name=giver_name, cpf=giver_cpf)
 
-sys.create_person(name=giver_name, cpf=giver_cpf)
-sys.create_person(name=receiver_name, cpf=receiver_cpf)
+    receiver_name = input("Digite o nome da pessoa que vai receber a transferencia: ")
+    receiver_cpf = int(input("Digite o CPF dessa pessoa: "))
+    sys.create_person(name=receiver_name, cpf=receiver_cpf)
 
-sys.show_status()
+    sys.show_status()
 
-sys.sys_open_account(giver_cpf, main_bank)
-sys.sys_open_account(receiver_cpf, main_bank)
-sys.sys_open_account(receiver_cpf, other_bank)
+    sys.sys_open_account(giver_cpf, main_bank)
+    sys.sys_open_account(receiver_cpf, main_bank)
+    sys.sys_open_account(receiver_cpf, other_bank)
 
-sys.sys_deposit(cpf=giver_cpf, bank=main_bank,
-                value=float(input(f"Qual valor que será depositado inicialmente na conta do {giver_name}?\n"
-                                  f"-> R$ ")))
+    sys.sys_deposit(cpf=giver_cpf, bank=main_bank,
+                    value=float(input(f"Qual valor que será depositado inicialmente na conta do {giver_name}?\n"
+                                      f"-> R$ ")))
 
-sys.make_transfer(value=float(input(f"Qual valor que será transferido para o {receiver_name} (mesmo banco)?\n"
-                                    f"-> R$ ")),
-                  origin_id=giver_cpf, origin_bank=main_bank, target_id=receiver_cpf, target_bank=main_bank)
+    sys.make_transfer(value=float(input(f"Qual valor que será transferido para o {receiver_name} (mesmo banco)?\n"
+                                        f"-> R$ ")),
+                      origin_id=giver_cpf, origin_bank=main_bank, target_id=receiver_cpf, target_bank=main_bank)
 
-sys.make_transfer(value=float(input(f"Qual valor que será transferido para o {receiver_name} (bancos diferentes) ?\n"
-                                    f"-> R$ ")),
-                  origin_id=giver_cpf, origin_bank=main_bank, target_id=receiver_cpf, target_bank=other_bank)
+    sys.make_transfer(
+        value=float(input(f"Qual valor que será transferido para o {receiver_name} (bancos diferentes) ?\n"
+                          f"-> R$ ")),
+        origin_id=giver_cpf, origin_bank=main_bank, target_id=receiver_cpf, target_bank=other_bank)
 
-sys.get_person_data(receiver_cpf)
-sys.get_person_data(giver_cpf)
+    sys.get_person_data(receiver_cpf)
+    sys.get_person_data(giver_cpf)
 
-sys.remove_person(giver_cpf)
-sys.remove_person(receiver_cpf)
-sys.remove_bank(name=main_bank)
+    sys.remove_person(giver_cpf)
+    sys.remove_person(receiver_cpf)
+    sys.remove_bank(name=main_bank)
 
-sys.show_status()
+    sys.show_status()
+
+print("\n\n\n\n\n")
 
 run = True
-current_state = 'menu'
+current_state = 'main_menu'
 while run:
-    if current_state == 'menu':
+
+    if current_state == 'main_menu':
         print("\n\n\nBem-vindo ao SUB, o que deseja fazer?")
         print("[0] - Encerrar.")
-        print("[1] - Cadastrar nova pessoa.")
-        print("[2] - Remover pessoa existente.")
-        print("[3] - Cadastrar novo banco.")
-        print("[4] - Remover banco existente.")
-        print("[5] - Criar conta para pessoa cadastrada.")
-        print("[6] - Realizar depósito em conta.")
-        print("[7] - Realizar saque de conta.")
-        print("[8] - Realizar transferência automatizada entre pessoas.")
-        print("[9] - Mostrar status do sistema.")
-        print("[10] - Mostrar dados de uma pessoa.")
+        print("[1] - Cadastro.")
+        print("[2] - Remoção.")
+        print("[3] - Financeiro.")
+        print("[4] - Informações.")
 
         option = int(input("-> "))
-
         if option == 0:
             run = False
             break
         elif option == 1:
+            current_state = 'sign_menu'
+        elif option == 2:
+            current_state = 'delete_menu'
+        elif option == 3:
+            current_state = 'transactions_menu'
+        elif option == 4:
+            current_state = 'info_menu'
+
+    elif current_state == 'sign_menu':
+        print()
+        print("[1] - Cadastrar nova pessoa.")
+        print("[2] - Cadastrar novo banco.")
+        print("[3] - Criar conta em banco para pessoa cadastrada.")
+        print("[4] - Voltar.")
+
+        option = int(input("-> "))
+
+        if option == 1:
             name = input("Digite o nome da pessoa: ")
             cpf = int(input("Digite o CPF dessa pessoa: "))
             sys.create_person(name=name, cpf=cpf)
+
         elif option == 2:
-            cpf = int(input("Digite o CPF dessa pessoa: "))
-            sys.remove_person(giver_cpf)
-        elif option == 3:
             bank = input("Digite o nome do banco a ser criado: ").strip()
             fee = float(int(input("Qual a taxa de transferência externa desse banco, em %: ")) / 100)
 
             sys.create_bank(name=bank, bank_fee=fee)
-        elif option == 4:
-            bank = input("Digite o nome do banco a ser removido: ").strip()
-            sys.remove_bank(name=bank)
-        elif option == 5:
+
+        elif option == 3:
             cpf = int(input("Digite o CPF da pessoa que abrirá a conta: "))
             bank = input("Digite o nome do banco onde a conta será criada: ").strip()
             sys.sys_open_account(cpf, bank)
-        elif option == 6:
+
+        elif option == 4:
+            current_state = 'main_menu'
+
+    elif current_state == 'delete_menu':
+        print()
+        print("[1] - Remover pessoa existente.")
+        print("[2] - Remover banco existente.")
+        print("[3] - Voltar.")
+
+        option = int(input("-> "))
+
+        if option == 1:
+            cpf = int(input("Digite o CPF dessa pessoa: "))
+            sys.remove_person(cpf)
+
+        elif option == 2:
+            bank = input("Digite o nome do banco a ser removido: ").strip()
+            sys.remove_bank(name=bank)
+
+        elif option == 3:
+            current_state = 'main_menu'
+
+    elif current_state == 'transactions_menu':
+        print()
+        print("[1] - Realizar depósito em conta.")
+        print("[2] - Realizar saque de conta.")
+        print("[3] - Realizar transferência automatizada entre pessoas.")
+        print("[4] - Voltar.")
+
+        option = int(input("-> "))
+
+        if option == 1:
             cpf = int(input("Digite o CPF dessa pessoa: "))
             bank = input("Digite o banco no qual deve ser feito o depósito:").strip()
             sys.sys_deposit(cpf=cpf, bank=bank,
-                            value=float(input(f"Qual valor que será depositado na conta do {sys.people[cpf].name}?\n"
+                            value=float(input(f"Qual valor que será depositado na conta?\n"
                                               f"-> R$ ")))
-        elif option == 7:
+        elif option == 2:
             cpf = int(input("Digite o CPF dessa pessoa: "))
             bank = input("Digite o banco no qual deve ser feito o saque:").strip()
             do_limit = True if input("O saque deve ser limitado?\n"
@@ -451,9 +503,9 @@ while run:
                                      "[n] - Não\n"
                                      "-> ") == 's' else False
             sys.sys_draw(cpf=cpf, bank=bank,
-                         value=float(input(f"Qual valor que será reitrado da conta do {sys.people[cpf].name}?\n"
+                         value=float(input(f"Qual valor que será reitrado da conta?\n"
                                            f"-> R$ ")), has_time_limit=do_limit)
-        elif option == 8:
+        elif option == 3:
             giver_cpf = int(input("Digite o cpf da pessoa que vai realizar a transferencia: "))
             giver_bank = input("Digite o banco no qual deve ser feito o saque:").strip()
 
@@ -466,13 +518,26 @@ while run:
                                      "-> ") == 's' else False
 
             sys.make_transfer(
-                value=float(input(f"Qual valor que será transferido para o {sys.people[receiver_cpf].name}\n"
+                value=float(input(f"Qual valor que será transferido?\n"
                                   f"-> R$ ")),
                 origin_id=giver_cpf, origin_bank=giver_bank, target_id=receiver_cpf, target_bank=receiver_bank,
                 is_time_limited=do_limit)
 
-        elif option == 9:
+        elif option == 4:
+            current_state = 'main_menu'
+
+    elif current_state == 'info_menu':
+        print()
+        print("[1] - Mostrar status do sistema.")
+        print("[2] - Mostrar dados de uma pessoa.")
+        print("[3] - Voltar.")
+
+        option = int(input("-> "))
+
+        if option == 1:
             sys.show_status()
-        elif option == 10:
+        elif option == 2:
             cpf = int(input("Digite o CPF dessa pessoa: "))
             sys.get_person_data(cpf)
+        elif option == 3:
+            current_state = 'main_menu'
